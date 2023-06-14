ae72186c524f33169f0b2f48407c27e705b578d8
Mustafa <yazilimcimiyim@gmail.com>
2023-06-14 23:13:35 +0300

    updated

--- src/main/java/day26polymorphism/AnimalRunner.java
+++ src/main/java/day26polymorphism/AnimalRunner.java
@@ -1,2 +1,9 @@
-package day26polymorphism;public class AnimalRunner {
+package day26polymorphism;
+
+public class AnimalRunner {
+    public static void main(String[] args) {
+        Cat myCat=new Cat();
+        myCat.eat();
+    }
 }
+
--- src/main/java/day26polymorphism/Cat.java
+++ src/main/java/day26polymorphism/Cat.java
@@ -1,4 +1,37 @@
-package day26polymorphizm;
+package day26polymorphism;
 
-public class Cat {
+public class Cat extends Animal{
+    public void meow(){
+        System.out.println("Cats meow..");
+    }
+    //Override annotation Override kurallarinin java tarafindan kontrol edilmesini saglar
+    @Override
+    public void eat(){
+        //Child daki override edilmis eat() methoduna Overriding Method  denir
+        System.out.println("Cats eat...");
+    }
+
+    @Override
+    public void drink() {
+        System.out.println("Cats drink..");
+    }
+
+    @Override
+    public Cat create() {
+        return new Cat();
+    }
+
+    @Override
+    public int add(int a, int b) {
+        return a+b;
+    }
+
+    @Override
+    public Integer multiply(Integer a, Integer b) {
+        return a*b;
+    }
+
+
+
+
 }
